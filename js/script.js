c9776f06180f26aeea2f38b41aa3532e08f179d7
Tim Hatch <timhatch2@gmail.com>
2021-02-27 14:24:00 -0600

    API call set up

--- js/script.js
+++ js/script.js
@@ -3,4 +3,30 @@ let pokeData, userInput;
 const $targetPokemon = $('#pokemon');
 const $idNumber = $('#id-number');
 const $moves = $('moves');
+const $input = $('input[type="text"]');
 
+$('form').on('submit', handleData);
+
+function handleData(event) {
+    event.preventDefault();
+
+    userInput = $input.val();
+
+    $.ajax({
+        url:'https://pokeapi.co/api/v2/pokemon/' + userInput
+    }).then(
+        (data) => {
+            pokeData = data;
+            render();
+        },
+        (error) => {
+            console.log('bad request', error)
+        }
+    );
+}
+
+function render() {
+    $targetPokemon.text(userInput);
+    $idNumber.text(pokeData.id);
+    //$moves.text(weatherData.main.feels_like);
+}
