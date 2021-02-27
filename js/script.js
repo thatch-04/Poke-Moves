180e647050a027e5b24b9c3beef7a677223a9443
Tim Hatch <timhatch2@gmail.com>
2021-02-27 16:47:35 -0600

    move API call created and DOM element loop created

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let pokeData, userInput;
+let pokeData, userInput, moveData;
 
 const $targetPokemon = $('#pokemon');
 const $idNumber = $('#id-number');
@@ -28,5 +28,41 @@ function handleData(event) {
 function render() {
     $targetPokemon.text(userInput);
     $idNumber.text(pokeData.id);
-    //$moves.text(weatherData.main.feels_like);
+    console.log(pokeData.moves[0].move.url);
+    /*pokeData.moves.forEach(move => {
+        console.log(move.url);
+        $.ajax({
+            url: `${move.url}`
+        }).then(
+            (data2) => {
+                moveData = data2;
+                console.log(moveData.effect_entries);
+                let $p = $('<p>').innertext(`${moveData.effect_entries[0].effect}`);
+                $('body').append($p);
+            },
+            (error) => {
+                console.log('bad request', error)
+            }
+        );
+    });*/
+
+        for (let i=0; i < pokeData.moves.length; i++) {
+        $.ajax({
+            url: pokeData.moves[i].move.url
+        }).then(
+            (data) => {
+                moveData = data;
+                console.log(moveData.effect_entries);
+                let $p = $('<p>').text(`${moveData.effect_entries[0].effect}`);
+                let $name = $('<p>').text(`${pokeData.moves[i].move.name}`)
+                $('body').append($name);
+                $('body').append($p);
+            },
+            (error) => {
+                console.log('bad request', error)
+            }
+        );
+
+    }
+
 }
